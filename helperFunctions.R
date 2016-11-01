fe337af2c584aed7a8a3a1ce4f982956b508a8f1
Snoopy1964 <ralf@ehret-family.com>
2016-11-01 10:02:28 +0100

    minor changes (comments)

--- helperFunctions.R
+++ helperFunctions.R
@@ -1,16 +1,20 @@
 calcBGdiffs <- function (l.BG) {
+# calculate the time difference to breakfast, lunch and dinner
   l.DBG   = c()
   l.DTime = c()
 
   for(i in 1:length(l.BG$Event)){
-    if(l.BG$Event[i] == "T2"){
+    # before Breakfast (T2)
+      if(l.BG$Event[i] == "T2"){
       l.BG.T2    = l.BG$BGLevel[i]
       l.Tstmp.T2 = l.BG$Tstmp[i]
     } 
+    # before Lunch (T4)
     if(l.BG$Event[i] == "T4"){
       l.BG.T4    = l.BG$BGLevel[i]
       l.Tstmp.T4 = l.BG$Tstmp[i]
     }
+    # before Dinner (T6)
     if(l.BG$Event[i] == "T6"){
       l.BG.T6    = l.BG$BGLevel[i]
       l.Tstmp.T6 = l.BG$Tstmp[i]
